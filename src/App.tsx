3bc9c98fc49db53c19af8ca6667e17b6f2819dfc
cm.kim@comtec.co.kr <cm.kim@comtec.co.kr>
2025-02-10 17:43:00 +0900

    - createTodoForm 연결함

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,16 @@ import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
 import { useAuthenticator } from '@aws-amplify/ui-react';
 
+import TodoCreateForm from "../ui-components/TodoCreateForm";
+
 const client = generateClient<Schema>();
 
 function App() {
   const { signOut } = useAuthenticator();
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
   const [companies, setCompanies] = useState<Array<Schema["Company"]["type"]>>([]);
+  
+  const [showModal, setShowModal] = useState(false); // 모달 상태 추가
 
   useEffect(() => {
     client.models.Todo.observeQuery().subscribe({
@@ -32,14 +36,13 @@ function App() {
 
   }, []);
 
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
-  }
+  // function createTodo() {
+  //   client.models.Todo.create({ content: window.prompt("Todo content") });
+  // }
 
   // function createTodo(content: string) {
   //   client.models.Todo.create({ content });
   // }
-
     
   function deleteTodo(id: string) {
     client.models.Todo.delete({ id })
@@ -48,7 +51,17 @@ function App() {
   return (
     <main>
       <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
+      {/* <button onClick={createTodo}>+ new</button> */}
+      {/* 모달 열기 버튼 */}
+      <button onClick={() => setShowModal(true)}>+ new</button>
+
+      {/* 모달이 열려 있을 때만 표시 */}
+      {showModal && (
+        <div className="modal">
+          <TodoCreateForm />
+          <button onClick={() => setShowModal(false)}>닫기</button>
+        </div>
+      )}
       <ul>
         {todos.map((todo) => (
           <li onClick={() => deleteTodo(todo.id)} key={todo.id}>{todo.content}</li>
